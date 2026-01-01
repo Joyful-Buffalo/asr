0f690a0138200e17c94eb0b1abe8adf57b018a9c
Joyful Buffalo <3072806964@qq.com>
2026-01-01 22:03:50 +0800

    bb

--- models/MHA.py
+++ models/MHA.py
@@ -19,6 +19,17 @@ class MutiHeadAttention(nn.Module):
         seq_len = position_idx.size(1)
 
         inv_freq = 1./(10000 ** torch.arange(0, self.head_dim, 2).float()/self.head_dim)
+        freqs = torch.einsum('bi,j->bij',position_idx.float(),inv_freq)
+        emb = torch.cat((freqs, freqs),dim=-1)
+        cos = torch.cos(emb).view(1,1,seq_len, self.head_dim)
+        sin = torch.sin(emb).view(1,1,seq_len,self.head_dim)
+        return (x*cos) + (self.rotate_half(x)*sin)
+    
+    def rotate_half(self,x):
+        x1, x2 = x.chunk(2, dim=-1)
+        return torch.cat((-x2, x1),dim=-1)
+    
+    
 
         
 
